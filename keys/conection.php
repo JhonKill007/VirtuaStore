de262966bf6f9ca3ef7e60ad59841f7cdae7341d
juan guzman <jrguzman@marzanmercado.com>
2023-03-07 18:19:33 -0400

    cadena de conexion

--- keys/conection.php
+++ keys/conection.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";
-$dbusername = "root";
-$dbpassword =  "Jrguzman23";
-$dbname = "tiendav";
+$host = "127.0.0.1:3306";
+$dbusername = "u399217347_tiendav";
+$dbpassword =  "u399217347_tiendav";
+$dbname = "Pas3w0rd@";
 $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
 
 
