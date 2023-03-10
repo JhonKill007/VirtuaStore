ccdde59a3408195638be72b08aeab5e6862f7b9b
juan guzman <jrguzman@marzanmercado.com>
2023-03-10 13:35:44 -0400

    error duplicidad en carrito

--- car.php
+++ car.php
@@ -148,75 +148,9 @@ if (isset($_SESSION['id'])) {
                     </script>
                 </div>
 
-                <div class="col-lg-4">
-                    <div style="border:1px solid; padding:10px">
-                        <div>
-                            <h4 style="margin-top: 15px;">Resumen Del Pedido</h4>
-                            <div class="d-flex flex-row justify-content-between" style="margin-top: 20px; margin-bottom: 20px;">
-                                <div class="col-sm-6">
-                                    <span>Subtotal:</span>
-                                </div>
-                                <div class="col-sm-6">
-                                    <span style="font-size: 25px;">$ <?php echo $total;  ?> </span>
-                                </div>
+               
 
-                            </div>
-
-                        </div>
-                        <div id="smart-button-container">
-                            <div style="text-align: center;">
-                                <div id="paypal-button-container"></div>
-                            </div>
-                        </div>
-                        <script src="https://www.paypal.com/sdk/js?client-id=sb&enable-funding=venmo&currency=USD" data-sdk-integration-source="button-factory"></script>
-                        <script>
-                            function initPayPalButton() {
-                                paypal.Buttons({
-                                    style: {
-                                        shape: 'rect',
-                                        color: 'gold',
-                                        layout: 'vertical',
-                                        label: 'paypal',
-
-                                    },
-
-                                    createOrder: function(data, actions) {
-                                        return actions.order.create({
-                                            purchase_units: [{
-                                                "amount": {
-                                                    "currency_code": "USD",
-                                                    "value": 1
-                                                }
-                                            }]
-                                        });
-                                    },
-
-                                    onApprove: function(data, actions) {
-                                        return actions.order.capture().then(function(orderData) {
-
-                                            // Full available details
-                                            console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
-
-                                            // Show a success message within this page, e.g.
-                                            const element = document.getElementById('paypal-button-container');
-                                            element.innerHTML = '';
-                                            element.innerHTML = '<h3>Thank you for your payment!</h3>';
-
-                                            // Or go to another URL:  actions.redirect('thank_you.html');
-
-                                        });
-                                    },
-
-                                    onError: function(err) {
-                                        console.log(err);
-                                    }
-                                }).render('#paypal-button-container');
-                            }
-                            initPayPalButton();
-                        </script>
-                    </div>
-
-                </div>
+              
 
 
 
